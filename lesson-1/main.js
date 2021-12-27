ea712f24e654c0ddba056e7fdd325742e22df6ec
Evgeniya Bayda <baje75128@gmail.com>
2021-12-27 08:48:55 +0200

    add

---
+++ lesson-1/main.js
@@ -0,0 +1,27 @@
+// console.log(process.env);
+//все,что мы пишем слева от node попадает в process.env
+// console.log(process.env.petro);
+// const { petro } = process.env;
+// console.log(petro);
+//все,что мы пишем справа от node попадает в process.argv
+
+// console.log(process.env)
+
+// console.log(process.env.petro)
+// console.log(process.env.mykola)
+// console.log(process.env.zheka)
+// const { petro } = process.env;
+// console.log(petro);
+// console.log(`Hellow world!`)
+
+// console.log(process.argv)
+// ========================================
+
+// require('./calc-basic.js')
+// require('./calc')
+// =======
+
+//  const {calculator}= require('./calculatorOOP')
+const { calculator } = require("./calculatorOOPStatic");
+
+//  console.log(calculator)
